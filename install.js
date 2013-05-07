195af1b278ee930d441bf8cd7f4ba274a6af9425
Ruben Vermeersch <ruben@savanne.be>
2013-05-07 10:40:21 +0200

    Try to install aapt three times, it sometimes fails.

--- install.js
+++ install.js
@@ -19,20 +19,29 @@ if (os.type() == 'Darwin') {
     throw new Error('Unknown OS!');
 }
 
-var url = "http://dl-ssl.google.com/android/repository/platform-tools_r16-" + platform + ".zip";
-var tempFile = "/tmp/platform-tools-" + (new Date().getTime()) + ".zip";
+function attemptDownload(attemptsLeft) {
+    var url = "http://dl-ssl.google.com/android/repository/platform-tools_r16-" + platform + ".zip";
+    var tempFile = "/tmp/platform-tools-" + (new Date().getTime()) + ".zip";
 
-var file = fs.createWriteStream(tempFile);
-var request = http.get(url, function(response) {
-    response.pipe(file);
-    response.on('end', function () {
-        exec("unzip -j -o " + tempFile + " platform-tools/aapt -d tools/", function (err) {
-            if (err) {
-                throw err;
-            }
-            fs.chmodSync('tools/aapt', '755');
-            fs.unlinkSync(tempFile);
-            process.exit();
+    var file = fs.createWriteStream(tempFile);
+    var request = http.get(url, function(response) {
+        response.pipe(file);
+        response.on('end', function () {
+            exec("unzip -j -o " + tempFile + " platform-tools/aapt -d tools/", function (err) {
+                if (err) {
+                    if (attemptsLeft === 0) {
+                        throw err;
+                    } else {
+                        attemptDownload(attemptsLeft - 1);
+                        return;
+                    }
+                }
+                fs.chmodSync('tools/aapt', '755');
+                fs.unlinkSync(tempFile);
+                process.exit();
+            });
         });
     });
-});
+}
+
+attemptDownload(3);
